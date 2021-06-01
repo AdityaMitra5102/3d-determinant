ded6abf87c5c4c2599c25a8b04397b7a182261d7
Cryptane Online <adityaarghya0@gmail.com>
2021-06-01 12:33:06 +0530

    Add files via upload

--- Decrypt.java
+++ Decrypt.java
@@ -1,3 +1,4 @@
+import java.math.*;
 import java.util.*;
 class Decrypt
 {
@@ -5,16 +6,16 @@ class Decrypt
     double b[]=new double[9];
     double x[]=new double[9];
     double k[]=new double[9];
-    double privateKey;
+    double otKey;
     static Scanner sc=new Scanner(System.in);
     void read()
     {
-        System.out.println("Enter public key 1");
+        System.out.println("Enter public key");
         for(int i=0;i<9;i++)
         {
             a[i]=sc.nextDouble();
         }
-        System.out.println("Enter public key 2");
+        System.out.println("Enter private key");
         for(int i=0;i<9;i++)
         {
             b[i]=sc.nextDouble();
@@ -24,8 +25,8 @@ class Decrypt
         {
             k[i]=sc.nextDouble();
         }
-        System.out.println("Enter private Key");
-        privateKey=sc.nextDouble();
+        System.out.println("Enter One time Key");
+        otKey=sc.nextDouble();
         decrypt();
     }
 
@@ -33,11 +34,8 @@ class Decrypt
     {
         double parityInput=k[0];
          k[0]=1;
-        x[0]=privateKey/((-k[0]*(a[4]*b[8]+a[5]*b[7]-a[7]*b[5]-a[8]*b[4]))+(+k[1]*(a[3]*b[8]+a[5]*b[6]-a[6]*b[5]-a[8]*b[3]))+(-k[2]*(a[3]*b[7]+a[4]*b[6]-a[6]*b[4]-a[7]*b[3]))+(+k[3]*(a[1]*b[8]+a[2]*b[7]-a[7]*b[2]-a[8]*b[1]))+(-k[4]*(a[0]*b[8]+a[2]*b[6]-a[6]*b[2]-a[8]*b[0]))+(+k[5]*(a[0]*b[7]+a[1]*b[6]-a[6]*b[1]-a[7]*b[0]))+(-k[6]*(a[1]*b[5]+a[2]*b[4]-a[4]*b[2]-a[5]*b[1]))+(+k[7]*(a[0]*b[5]+a[2]*b[3]-a[3]*b[2]-a[5]*b[0]))+(-k[8]*(a[0]*b[4]+a[1]*b[3]-a[3]*b[1]-a[4]*b[0])));
-       
-        //System.out.println("Private key"+privateKey);
-        
-        //k[0]=1;
+        x[0]=otKey/((-k[0]*(a[4]*b[8]+a[5]*b[7]-a[7]*b[5]-a[8]*b[4]))+(+k[1]*(a[3]*b[8]+a[5]*b[6]-a[6]*b[5]-a[8]*b[3]))+(-k[2]*(a[3]*b[7]+a[4]*b[6]-a[6]*b[4]-a[7]*b[3]))+(+k[3]*(a[1]*b[8]+a[2]*b[7]-a[7]*b[2]-a[8]*b[1]))+(-k[4]*(a[0]*b[8]+a[2]*b[6]-a[6]*b[2]-a[8]*b[0]))+(+k[5]*(a[0]*b[7]+a[1]*b[6]-a[6]*b[1]-a[7]*b[0]))+(-k[6]*(a[1]*b[5]+a[2]*b[4]-a[4]*b[2]-a[5]*b[1]))+(+k[7]*(a[0]*b[5]+a[2]*b[3]-a[3]*b[2]-a[5]*b[0]))+(-k[8]*(a[0]*b[4]+a[1]*b[3]-a[3]*b[1]-a[4]*b[0])));
+   
         for(int i=1;i<9;i++)
         {
             x[i]=k[i]*x[0];
@@ -53,15 +51,16 @@ class Decrypt
 
     void show()
     {
-        if(privateKey==0)
+        if(otKey==0)
         {
             System.out.println("\nError! Either public keys are invalid or this cipher cannot be decrypted with these public keys on this algorithm. Try with different public keys.");
             return;
         }
         System.out.println("\nShowing Clear matrix");
+        
         for(int i=0;i<9;i++)
         {
-            System.out.print(x[i]+" ");
+            System.out.print(new BigDecimal(String.valueOf(x[i])).setScale(5, BigDecimal.ROUND_HALF_UP)+" ");
             if((i+1)%3==0)
             {
                 System.out.println();
--- Encrypt.java
+++ Encrypt.java
@@ -5,16 +5,16 @@ class Encrypt
     double b[]=new double[9];
     double x[]=new double[9];
     double k[]=new double[9];
-    double privateKey;
+    double otKey;
     static Scanner sc=new Scanner(System.in);
     void read()
     {
-        System.out.println("Enter public key 1");
+        System.out.println("Enter public key");
         for(int i=0;i<9;i++)
         {
             a[i]=sc.nextDouble();
         }
-        System.out.println("Enter public key 2");
+        System.out.println("Enter private key");
         for(int i=0;i<9;i++)
         {
             b[i]=sc.nextDouble();
@@ -33,20 +33,17 @@ class Encrypt
         {
             k[i]=x[i]/x[0];
         }
-        privateKey=x[0]*((-k[0]*(a[4]*b[8]+a[5]*b[7]-a[7]*b[5]-a[8]*b[4]))+(+k[1]*(a[3]*b[8]+a[5]*b[6]-a[6]*b[5]-a[8]*b[3]))+(-k[2]*(a[3]*b[7]+a[4]*b[6]-a[6]*b[4]-a[7]*b[3]))+(+k[3]*(a[1]*b[8]+a[2]*b[7]-a[7]*b[2]-a[8]*b[1]))+(-k[4]*(a[0]*b[8]+a[2]*b[6]-a[6]*b[2]-a[8]*b[0]))+(+k[5]*(a[0]*b[7]+a[1]*b[6]-a[6]*b[1]-a[7]*b[0]))+(-k[6]*(a[1]*b[5]+a[2]*b[4]-a[4]*b[2]-a[5]*b[1]))+(+k[7]*(a[0]*b[5]+a[2]*b[3]-a[3]*b[2]-a[5]*b[0]))+(-k[8]*(a[0]*b[4]+a[1]*b[3]-a[3]*b[1]-a[4]*b[0])));
-
-        //System.out.println("Private key"+privateKey);
-
-        
+        otKey=x[0]*((-k[0]*(a[4]*b[8]+a[5]*b[7]-a[7]*b[5]-a[8]*b[4]))+(+k[1]*(a[3]*b[8]+a[5]*b[6]-a[6]*b[5]-a[8]*b[3]))+(-k[2]*(a[3]*b[7]+a[4]*b[6]-a[6]*b[4]-a[7]*b[3]))+(+k[3]*(a[1]*b[8]+a[2]*b[7]-a[7]*b[2]-a[8]*b[1]))+(-k[4]*(a[0]*b[8]+a[2]*b[6]-a[6]*b[2]-a[8]*b[0]))+(+k[5]*(a[0]*b[7]+a[1]*b[6]-a[6]*b[1]-a[7]*b[0]))+(-k[6]*(a[1]*b[5]+a[2]*b[4]-a[4]*b[2]-a[5]*b[1]))+(+k[7]*(a[0]*b[5]+a[2]*b[3]-a[3]*b[2]-a[5]*b[0]))+(-k[8]*(a[0]*b[4]+a[1]*b[3]-a[3]*b[1]-a[4]*b[0])));
+       
         k[0] = Double.longBitsToDouble(Double.doubleToRawLongBits(k[1]) ^ Double.doubleToRawLongBits(k[2])^ Double.doubleToRawLongBits(k[3])^ Double.doubleToRawLongBits(k[4])^ Double.doubleToRawLongBits(k[5])^ Double.doubleToRawLongBits(k[6])^ Double.doubleToRawLongBits(k[7])^ Double.doubleToRawLongBits(k[8]));
         show();
     }
 
     void show()
     {
-        if(privateKey==0)
+        if(otKey==0)
         {
-            System.out.println("\nError! Either public keys are invalid or this matrix cannot be encrypted with these public keys on this algorithm. Try with different public keys.");
+            System.out.println("\nError! Either keys are invalid or this matrix cannot be encrypted with these public keys on this algorithm. Try with different keys.");
             return;
         }
         System.out.println("\nShowing Cipher matrix");
@@ -58,7 +55,7 @@ class Encrypt
                 System.out.println();
             }
         }
-        System.out.println("\nShowing privateKey: "+privateKey);
+        System.out.println("\nShowing one time key: "+otKey);
     }
     
     public static void main(String args[])
